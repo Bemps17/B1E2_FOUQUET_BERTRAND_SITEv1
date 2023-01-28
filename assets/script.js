e0d562cf17a0e4411ac168d12847eab255214b52
Bemps17 <116157699+Bemps17@users.noreply.github.com>
2023-01-28 17:12:59 +0100

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,21 @@ scrollTop.addEventListener("click", function() {
   });
 });
 
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('send', 'pageview');
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { window.dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'GA_TRACKING_ID');
+
+
+
